5eb7d1b88e5dd05dc0e31fce4914b11b6919e435
Lukas Frei <lukas.frei@zuehlke.com>
2021-12-31 16:25:42 +0100

    Add cors for survey-service

--- survey-service/app.js
+++ survey-service/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const kafka = require('./kafka')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors());
+app.options('*', cors());
 
 app.get('/survey/:id', (req, res) => {
     const survey = kafka.surveys.get(req.params.id)
